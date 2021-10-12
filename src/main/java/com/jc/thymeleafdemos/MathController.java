3b112566fc1c73f9a0e7d9b8a5b17771f47b3f44
José Cuzmar <jose.cuzmar.zamorano@ciisa.cl>
2021-10-12 09:46:44 -0300

    pruebas mate

---
+++ src/main/java/com/jc/thymeleafdemos/MathController.java
@@ -0,0 +1,21 @@
+package com.jc.thymeleafdemos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MathController {
+
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("message", "Hola alumno");
+		return "index";
+	}
+
+	@GetMapping("/mat/aritmetica/sumas")
+	public String sumas(Model model) {
+		model.addAttribute("message", "Hola alumno");
+		return "/mat/aritmetica/sumas";
+	}
+}
